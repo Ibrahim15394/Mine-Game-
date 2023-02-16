3d843753eeb955687fb999de86cd42a13089455d
ibrahimmorsy <ibrahimmorsy@matrixzone.net>
2023-02-16 19:05:02 +0200

    landscape mode ui

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ const [userNumber, setUserNumber] = useState();
 const [gameIsOver, setGameIsOver] = useState(true);
 const [guessRounds, setGuessRounds] = useState(0);
 
+
 const [fontsLoaded] = useFonts({
   'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
   'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf') 
